d1b080df9f8017ef85450d79c8e91d6808a56643
ElvisLee <elvislee0725@gmail.com>
2026-08-16 19:57:13 -0700

    Point Download CV button at Google Drive resume, drop unused placeholder PDF

--- src/components/About/DownloadCvButton.jsx
+++ src/components/About/DownloadCvButton.jsx
@@ -1,7 +1,12 @@
 function DownloadCvButton() {
   return (
     <div className="text-center mt-4">
-      <a className="btn btn--download" href="/assets/cv.pdf">
+      <a
+        className="btn btn--download"
+        href="https://drive.google.com/file/d/1zbxJVNfewrog37zcP6B5XWIJ9ZG0vXWu/view?usp=drive_link"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <i className="fas fa-file-download"></i>&nbsp;Download CV
       </a>
     </div>
